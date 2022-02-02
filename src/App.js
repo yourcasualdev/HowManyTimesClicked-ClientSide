cbbff9bb55443cc9c87b0a8e3272650c8cbdf4d1
Babal <babalibrahim34@gmail.com>
2022-02-02 22:44:42 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from './Counter'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const API_URL = 'https://how-many-times-clicked.herokuapp.com/num';
+
+  const [num, setNum] = useState('');
+
+
+
+  useEffect(() => {
+    const fetchNum = async () => {
+      const response = await fetch(API_URL);
+      setNum(await response.json());
+    }
+    fetchNum();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter num={num} />
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,9 @@
+const Counter = ({ num }) => {
+    return (
+        <div className="counter">
+            <p>{num}</p>
+        </div>
+    );
+};
+
+export default Counter;
